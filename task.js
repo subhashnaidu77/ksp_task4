4db7112fd71db16831f66db3cb12757ac9eb3ca2
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2024-01-26 16:43:59 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,61 @@
+console.log("Hello world");
+window.alert("Welcomre to the form to fill the form | Click ok to fill it");
+// now it is about variables
+variable1=1;
+vaiable2=9;
+console.log("variable2");
+console.log(variable1);
+myName=24;
+subhash_naidu= 20500;
+console.log(subhash_naidu);
+console.log(myName);
+// 
+
+demo=12;
+demo=56;
+console.log(demo);
+//proper way of declaring variable 
+let vari=28;
+let var1=72;
+const variable3=89;
+console.log(vari);
+console.log(variable3);
+// operators
+console.log(vari+vari);
+console.log(var1-variable1);
+console.log(var1/vaiable2);
+console.log(var1%vaiable2);
+console.log(var1*vari);
+console.log(var1**4);
+let number1=25;
+number1 +=3;
+console.log(number1);
+let number2=28;
+console.log(number2++);
+let number3=21;
+console.log(++number3);
+let number4=90;
+console.log(--number4);
+let number5=98;
+console.log(++number5);
+let number6=99;
+if(number6>=99){
+    console.log("yes it is true");
+}
+let number7 =88;
+if(number7>1 && number6>10){
+    if(number1>=1 || number5<=100){
+        console.log("True");
+    }
+}
+else{
+    console.log("false");
+}
+
+for(number8=0;number8<10;number8++){
+    for(number9=1;number9<5;number9++){
+        console.log("Welcome to this page");
+    }
+}
+
+
